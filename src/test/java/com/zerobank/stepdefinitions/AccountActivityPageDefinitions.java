2a4daf8cd17df48203e3a596b1b9f7d6e5c2f2b7
Harun1233 <av.harunonat@gmail.com>
2022-02-13 15:27:23 +0000

    sth

---
+++ src/test/java/com/zerobank/stepdefinitions/AccountActivityPageDefinitions.java
@@ -0,0 +1,24 @@
+package com.zerobank.stepdefinitions;
+
+import io.cucumber.java.en.Then;
+
+public class AccountActivityPageDefinitions {
+
+    @Then("account dropdown should have {string} as a default option")
+    public void account_dropdown_should_have_as_a_default_option(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("account dropdown should have {string} options")
+    public void account_dropdown_should_have_options(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("transaction table should have {string} as column name")
+    public void transaction_table_should_have_as_column_name(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ target/test-classes/features/login.feature
@@ -0,0 +1,17 @@
+
+Feature: Users should be able to login only with valid credentials
+
+
+  Background:
+    Given user is on the login page
+
+
+  Scenario:
+    Given user enters valid credentials
+    Then user should be able to login
+
+  Scenario:
+    Given user enters invalid credentials
+    Then user should see "Login and/or password are wrong." message
+
+
